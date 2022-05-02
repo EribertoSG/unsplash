e84a44427cb94f586c7b180a66638269eb955d23
Eriberto SG <santosbxs1999@gmail.com>
2022-05-01 23:20:34 -0500

    Se creo la estructura del hero-image al igual que el input search.

---
+++ components/Organisms/HeroImage.jsx
@@ -0,0 +1,31 @@
+import Inputsearch from "../molecules/InputSearch"
+
+const Heroimage = () => {
+    return (
+        <div className="hero-image">
+            <img src="/assets/img/agnieszka-kowalczyk.jpg" alt="agnieszka-kowalczyk" className="background-image" />
+            <div className="hero-image__container">
+                <div className="hero-image__content">
+                    <div className="hero-image__main-content">
+                        <div className="hero-image__title-text">
+                            <h1 className="hero-image__title">Unsplash</h1>
+                            <div className="hero-image__text">
+                                <p>The internet’s source of freely-usable images.</p>
+                                <p>Powered by creators everywhere.</p>
+                            </div>
+                        </div>
+                        <div className="hero-image__search">
+                            <Inputsearch />
+                        </div>
+                    </div>
+                </div>
+                <div className="hero-image__footer">
+                    <p>Photo of the Day byAgnieszka Kowalczyk</p>
+                    <p>Read more about the Unsplash License</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Heroimage
