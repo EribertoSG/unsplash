1728eb95bbf5d3d25974f66030ef96d192bede38
Eriberto SG <santosbxs1999@gmail.com>
2022-05-18 09:51:33 -0500

    Se estructuro y diseno el loader.

---
+++ components/atoms/Loading.jsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+        <div className="loadingio-spinner-rolling-jnb6418tp2s">
+            <div className="ldio-hnss69l0ql9">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
