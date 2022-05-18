131e3a078bf099958747372960170948b9006f27
Eriberto SG <santosbxs1999@gmail.com>
2022-05-18 09:59:19 -0500

    Se hizo la peticion a la API de unsplash para traer una lista de fotos, ademas de traer una foto de forma aleatoria.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
-import Heroimage from "../components/organisms/HeroImage";
-import Layout from "../components/pages/Layout";
-import AllPhotos from "../components/template/AllPhotos";
+import axios from "axios"
+import useSearch from "../components/hooks/useSearch"
+import Heroimage from "../components/organisms/HeroImage"
+import Layout from "../components/pages/Layout"
+import AllPhotos from "../components/template/AllPhotos"
 
-export default function Home() {
+export default function Home({ listPhotos, getARandomPhoto }) {
+  const { photos } = useSearch()
+
   return (
     <>
-      <Heroimage />
+      <Heroimage src={getARandomPhoto} />
       <Layout>
-        <AllPhotos />
+        <AllPhotos photos={listPhotos} />
       </Layout>
     </>
   )
+}
+
+export const getServerSideProps = async (ctx) => {
+  const headers = {
+    headers: {
+      Authorization: 'Client-ID I22cKyDJTTGNBp_cNmTJ-syXQtbP5OVIwLtVY2eg2to'
+    }
+  }
+  const response = await axios.get('https://api.unsplash.com/photos/', headers)
+  const responseTwo = await axios.get('https://api.unsplash.com/photos/random', headers)
+  const listPhotos = await response.data
+  const getARandomPhoto = await responseTwo.data
+  return {
+    props: {
+      listPhotos,
+      getARandomPhoto
+    }
+  }
+
 }
