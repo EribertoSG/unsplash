2614f5d02739f1a861a9e4a57780ef441aefca55
Eriberto SG <santosbxs1999@gmail.com>
2022-05-18 09:53:25 -0500

    Se estructuro el result search.

--- components/pages/ResultSearch.jsx
+++ components/pages/ResultSearch.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import AllPhotos from "../template/AllPhotos"
 
 const ResultSearch = () => {
+    
     return (
         <div>
             <h3>Results for girl on Unsplash API</h3>
