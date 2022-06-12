9bbd165719500fad8b66c00bc95cc97e616abddf
EribertoSG <fordbxs@gmail.com>
2022-06-12 14:10:48 -0500

    Se creo el efecto overlay en las photos.

--- components/template/AllPhotos.jsx
+++ components/template/AllPhotos.jsx
@@ -1,6 +1,17 @@
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import Text from '../atoms/Text'
+import UserProfile from '../atoms/UserProfile'
 
-const AllPhotos = ({ photos }) => {
+const AllPhotos = () => {
+    const photos = [
+        { id: 1, url: '/assets/img/agnieszka-kowalczyk.jpg' },
+        { id: 2, url: '/assets/img/ayo-ogunseinde-6W4F62sN_yI-unsplash.jpg' },
+        { id: 3, url: '/assets/img/elijah-m-henderson.jpg' },
+        { id: 4, url: '/assets/img/fabrizio-lunardi.jpg' },
+        { id: 5, url: '/assets/img/hamid-tajik.jpg' },
+        { id: 6, url: '/assets/img/marc-kleen.jpg' }
+    ]
+
     const breakPoints = {
         350: 1,
         750: 2,
@@ -10,11 +21,28 @@ const AllPhotos = ({ photos }) => {
         <div className='masonry'>
             <ResponsiveMasonry columnsCountBreakPoints={breakPoints}>
                 <Masonry>
-                    {
+                    {/* {
                         photos &&
                         photos.map(image => (
                             <div key={image.id} className='masonry-item'>
                                 <img src={image.urls.regular} alt="masonry" />
+                            </div>
+                            ))
+                        } */}
+
+                    {
+                        photos.map(img => (
+                            <div key={img.id} className='masonry-item'>
+                                <img src={img.url} alt="masonry" />
+                                <div className='overlay'>
+                                    <div className="overlay__actions">
+                                        <button className="button">Dowload</button>
+                                    </div>
+                                    <div className='overlay__user'>
+                                        <UserProfile />
+                                        <Text text="Eriberto Santos" className="user-text"/>
+                                    </div>
+                                </div>
                             </div>
                         ))
                     }
